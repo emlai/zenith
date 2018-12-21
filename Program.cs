11a1d5302c017f5e7c6108090ce92ebce0751992
emlai <emil@cs.helsinki.fi>
2018-12-21 22:35:26 +0200

    Initialize C# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zenith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
