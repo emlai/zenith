98f5baca3e7f342b3bda0df3f20a64145203410d
emlai <emil@cs.helsinki.fi>
2018-12-22 01:20:28 +0200

    Update area.cs

--- src/main/area.cs
+++ src/main/area.cs
@@ -1,82 +1,71 @@
 class Area
 {
-    Area(World world, Vector2 position, int level);
-    Area(SaveFile file, World world, Vector2 position, int level);
-    void save(SaveFile file);
-
-private:
-    Tile getRandomTile();
-    Tile getRandomTile();
-    World getWorld() { return world; }
-    Vector2 getPosition() { return position; }
-    const int size = 64;
-    const Vector2 sizeVector;
-
-    Tile getTileAt(Vector2 position);
-    Tile getTileAt(Vector2 position);
-
     List<Tile> tiles;
     World world;
     Vector2 position;
-}
-const Vector2 Area::sizeVector = Vector2(Area::size, Area::size);
+    const int size = 64;
+    const Vector2 sizeVector = Vector2(size, size);
 
-Area::Area(World world, Vector2 position, int level)
-:   world(world), position(position)
-{
-    tiles.reserve(size * size);
-    string groundId = level < 0 ? "DirtFloor" : "Grass";
+    World getWorld() { return world; }
+    Vector2 getPosition() { return position; }
 
-    for (Vector2 pos(0, 0); pos.y < size; ++pos.y)
+    Area(World world, Vector2 position, int level)
+    :   world(world), position(position)
     {
-        for (pos.x = 0; pos.x < size; ++pos.x)
-        {
-            tiles.emplace_back(world, position * sizeVector + pos, level, groundId);
+        tiles.reserve(size * size);
+        string groundId = level < 0 ? "DirtFloor" : "Grass";
 
-            if (level < 0)
-                tiles.back().setObject(new Object("Ground"));
+        for (Vector2 pos(0, 0); pos.y < size; ++pos.y)
+        {
+            for (pos.x = 0; pos.x < size; ++pos.x)
+            {
+                tiles.emplace_back(world, position * sizeVector + pos, level, groundId);
+
+                if (level < 0)
+                    tiles.back().setObject(new Object("Ground"));
+            }
         }
     }
-}
 
-Area::Area(SaveFile file, World world, Vector2 position, int level)
-:   world(world), position(position)
-{
-    tiles.reserve(size * size);
-
-    for (Vector2 pos(0, 0); pos.y < size; ++pos.y)
+    Area(SaveFile file, World world, Vector2 position, int level)
+    :   world(world), position(position)
     {
-        for (pos.x = 0; pos.x < size; ++pos.x)
-            tiles.emplace_back(file, world, position * sizeVector + pos, level);
+        tiles.reserve(size * size);
+
+        for (Vector2 pos(0, 0); pos.y < size; ++pos.y)
+        {
+            for (pos.x = 0; pos.x < size; ++pos.x)
+                tiles.emplace_back(file, world, position * sizeVector + pos, level);
+        }
     }
-}
 
-void Area::save(SaveFile file)
-{
-    foreach (var tile in tiles)
-        tile.save(file);
-}
+    void save(SaveFile file)
+    {
+        foreach (var tile in tiles)
+            tile.save(file);
+    }
 
-Tile Area::getTileAt(Vector2 position)
-{
-    assert(position.x >= 0 && position.x < size);
-    assert(position.y >= 0 && position.y < size);
-    return tiles[position.x + size * position.y];
-}
+    Tile getTileAt(Vector2 position)
+    {
+        assert(position.x >= 0 && position.x < size);
+        assert(position.y >= 0 && position.y < size);
+        return tiles[position.x + size * position.y];
+    }
 
-Tile Area::getTileAt(Vector2 position)
-{
-    assert(position.x >= 0 && position.x < size);
-    assert(position.y >= 0 && position.y < size);
-    return tiles[position.x + size * position.y];
-}
+    Tile getTileAt(Vector2 position)
+    {
+        assert(position.x >= 0 && position.x < size);
+        assert(position.y >= 0 && position.y < size);
+        return tiles[position.x + size * position.y];
+    }
 
-Tile Area::getRandomTile()
-{
-    return getTileAt(makeRandomVector(sizeVector));
-}
+    Tile getRandomTile()
+    {
+        return getTileAt(makeRandomVector(sizeVector));
+    }
 
-Tile Area::getRandomTile()
-{
-    return getTileAt(makeRandomVector(sizeVector));
+    Tile getRandomTile()
+    {
+        return getTileAt(makeRandomVector(sizeVector));
+    }
 }
