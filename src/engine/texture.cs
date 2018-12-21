05a362bf5236ef89b1a2dc196b5d27bc80ead058
emlai <emil@cs.helsinki.fi>
2018-12-21 23:56:43 +0200

    Update texture.cs

--- src/engine/texture.cs
+++ src/engine/texture.cs
@@ -1,161 +1,149 @@
 class Texture
 {
-    Texture(string fileName, Color32 transparentColor = Color32::none);
-    Texture(uint pixelFormat, Vector2 size);
-    void setBlendMode(bool);
-    void setColor(Color32);
-    void render(Window window, Vector2 position, Vector2 size = Vector2::zeroVector);
-    void render(Window window, Rect target);
-    void render(Window window, Rect source, Rect target);
-    void render(Window window, Rect source, Rect target, Color32 materialColor);
-    Vector2 getSize();
-    int getWidth();
-    int getHeight();
-    SDL_Surface getSurface() { return surface.get(); }
+    struct PixelFormatMasks
+    {
+        uint red, green, blue, alpha;
+    }
+
+    static PixelFormatMasks pixelFormatEnumToMasks(uint pixelFormat)
+    {
+        int bpp;
+        PixelFormatMasks masks;
+
+        if (!SDL_PixelFormatEnumToMasks(pixelFormat, bpp, masks.red, masks.green, masks.blue, masks.alpha))
+            throw std::runtime_error("SDL_PixelFormatEnumToMasks: invalid pixel format");
+
+        return masks;
+    }
+
+    static SDL_Surface createSurfaceWithFormat(uint pixelFormat, Vector2 size)
+    {
+        PixelFormatMasks masks = pixelFormatEnumToMasks(pixelFormat);
+        return SDL_CreateRGBSurface(0, size.x, size.y, SDL_BITSPERPIXEL(pixelFormat),
+                                    masks.red, masks.green, masks.blue, masks.alpha);
+    }
+
+    Texture(string fileName, Color32 transparentColor)
+    :   surface(SDL_LoadBMP(fileName.to_string().c_str()), SDL_FreeSurface)
+    {
+        if (!surface)
+            throw std::runtime_error("Unable to load " + fileName + ": " + SDL_GetError());
+
+        if (var converted = SDL_ConvertSurfaceFormat(surface.get(), SDL_PIXELFORMAT_RGBA8888, 0))
+            surface.reset(converted);
+
+        if (transparentColor)
+        {
+            var colorKey = SDL_MapRGB(surface->format,
+                                       static_cast<byte>(transparentColor.getRed()),
+                                       static_cast<byte>(transparentColor.getGreen()),
+                                       static_cast<byte>(transparentColor.getBlue()));
+            SDL_SetColorKey(surface.get(), 1, colorKey);
+        }
+
+        setBlendMode(true);
+    }
+
+    Texture(uint pixelFormat, Vector2 size)
+    :   surface(createSurfaceWithFormat(pixelFormat, size), SDL_FreeSurface)
+    {
+        setBlendMode(true);
+    }
+
+    void setBlendMode(bool state)
+    {
+        SDL_SetSurfaceBlendMode(surface.get(), state ? SDL_BLENDMODE_BLEND : SDL_BLENDMODE_NONE);
+    }
+
+    void render(Window window, Vector2 position, Vector2 size)
+    {
+        if (size.isZero())
+            size = getSize();
+
+        render(window, Rect(Vector2::zeroVector, size), Rect(position, size));
+    }
+
+    void render(Window window, Rect target)
+    {
+        render(window, Rect(Vector2::zeroVector, getSize()), target);
+    }
+
+    void render(Window window, Rect source, Rect target)
+    {
+        target = window.context.mapToTargetCoordinates(target);
+
+        SDL_BlitSurface(surface.get(),
+                        reinterpret_cast<SDL_Rect>(source),
+                        window.context.targetTexture.getSurface(),
+                        reinterpret_cast<SDL_Rect>(target));
+    }
+
+    // TODO: Move this functionality out of the engine to the game.
+    void render(Window window, Rect source, Rect target, Color32 materialColor)
+    {
+        target = window.context.mapToTargetCoordinates(target);
+
+        SDL_Surface targetSurface = window.context.targetTexture.getSurface();
+        const uint sourcePixels = static_cast<const uint>(surface->pixels);
+        uint targetPixels = static_cast<uint>(targetSurface->pixels);
+        var sourceWidth = surface->w;
+        var targetWidth = targetSurface->w;
+
+        uint transparentColor;
+        if (SDL_GetColorKey(surface.get(), transparentColor) != 0)
+            transparentColor = 0;
+
+        for (var y = source.getTop(); y <= source.getBottom(); ++y)
+        {
+            for (var x = source.getLeft(); x <= source.getRight(); ++x)
+            {
+                uint pixel = sourcePixels[y * sourceWidth + x];
+
+                if (pixel == transparentColor)
+                    continue;
+
+                const byte abgr = reinterpret_cast<const byte>(sourcePixels[y * sourceWidth + x]);
+                bool isMagenta = abgr[3] > 0 && abgr[3] == abgr[1] && abgr[2] == 0;
+
+                if (isMagenta)
+                {
+                    var brightness = abgr[3] / 128.0;
+                    pixel = (materialColor * brightness).value;
+                }
+
+                var targetY = y - source.getTop() + target.getTop();
+                var targetX = x - source.getLeft() + target.getLeft();
+                targetPixels[targetY * targetWidth + targetX] = pixel;
+            }
+        }
+    }
+
+    Vector2 getSize()
+    {
+        return Vector2(surface->w, surface->h);
+    }
+
+    int getWidth()
+    {
+        return surface->w;
+    }
+
+    int getHeight()
+    {
+        return surface->h;
+    }
+
+    void setColor(Color32 color)
+    {
+        SDL_SetSurfaceColorMod(surface.get(),
+                               byte(color.getRed()),
+                               byte(color.getGreen()),
+                               byte(color.getBlue()));
+        SDL_SetSurfaceAlphaMod(surface.get(),
+                               byte(color.getAlpha()));
+    }
 
 private:
     std::unique_ptr<SDL_Surface, void (*)(SDL_Surface)> surface;
     List<Color32> pixelData;
-}
-struct PixelFormatMasks
-{
-    uint red, green, blue, alpha;
-}
-
-static PixelFormatMasks pixelFormatEnumToMasks(uint pixelFormat)
-{
-    int bpp;
-    PixelFormatMasks masks;
-
-    if (!SDL_PixelFormatEnumToMasks(pixelFormat, bpp, masks.red, masks.green, masks.blue, masks.alpha))
-        throw std::runtime_error("SDL_PixelFormatEnumToMasks: invalid pixel format");
-
-    return masks;
-}
-
-static SDL_Surface createSurfaceWithFormat(uint pixelFormat, Vector2 size)
-{
-    PixelFormatMasks masks = pixelFormatEnumToMasks(pixelFormat);
-    return SDL_CreateRGBSurface(0, size.x, size.y, SDL_BITSPERPIXEL(pixelFormat),
-                                masks.red, masks.green, masks.blue, masks.alpha);
-}
-
-Texture::Texture(string fileName, Color32 transparentColor)
-:   surface(SDL_LoadBMP(fileName.to_string().c_str()), SDL_FreeSurface)
-{
-    if (!surface)
-        throw std::runtime_error("Unable to load " + fileName + ": " + SDL_GetError());
-
-    if (var converted = SDL_ConvertSurfaceFormat(surface.get(), SDL_PIXELFORMAT_RGBA8888, 0))
-        surface.reset(converted);
-
-    if (transparentColor)
-    {
-        var colorKey = SDL_MapRGB(surface->format,
-                                   static_cast<byte>(transparentColor.getRed()),
-                                   static_cast<byte>(transparentColor.getGreen()),
-                                   static_cast<byte>(transparentColor.getBlue()));
-        SDL_SetColorKey(surface.get(), 1, colorKey);
-    }
-
-    setBlendMode(true);
-}
-
-Texture::Texture(uint pixelFormat, Vector2 size)
-:   surface(createSurfaceWithFormat(pixelFormat, size), SDL_FreeSurface)
-{
-    setBlendMode(true);
-}
-
-void Texture::setBlendMode(bool state)
-{
-    SDL_SetSurfaceBlendMode(surface.get(), state ? SDL_BLENDMODE_BLEND : SDL_BLENDMODE_NONE);
-}
-
-void Texture::render(Window window, Vector2 position, Vector2 size)
-{
-    if (size.isZero())
-        size = getSize();
-
-    render(window, Rect(Vector2::zeroVector, size), Rect(position, size));
-}
-
-void Texture::render(Window window, Rect target)
-{
-    render(window, Rect(Vector2::zeroVector, getSize()), target);
-}
-
-void Texture::render(Window window, Rect source, Rect target)
-{
-    target = window.context.mapToTargetCoordinates(target);
-
-    SDL_BlitSurface(surface.get(),
-                    reinterpret_cast<SDL_Rect>(source),
-                    window.context.targetTexture.getSurface(),
-                    reinterpret_cast<SDL_Rect>(target));
-}
-
-// TODO: Move this functionality out of the engine to the game.
-void Texture::render(Window window, Rect source, Rect target, Color32 materialColor)
-{
-    target = window.context.mapToTargetCoordinates(target);
-
-    SDL_Surface targetSurface = window.context.targetTexture.getSurface();
-    const uint sourcePixels = static_cast<const uint>(surface->pixels);
-    uint targetPixels = static_cast<uint>(targetSurface->pixels);
-    var sourceWidth = surface->w;
-    var targetWidth = targetSurface->w;
-
-    uint transparentColor;
-    if (SDL_GetColorKey(surface.get(), transparentColor) != 0)
-        transparentColor = 0;
-
-    for (var y = source.getTop(); y <= source.getBottom(); ++y)
-    {
-        for (var x = source.getLeft(); x <= source.getRight(); ++x)
-        {
-            uint pixel = sourcePixels[y * sourceWidth + x];
-
-            if (pixel == transparentColor)
-                continue;
-
-            const byte abgr = reinterpret_cast<const byte>(sourcePixels[y * sourceWidth + x]);
-            bool isMagenta = abgr[3] > 0 && abgr[3] == abgr[1] && abgr[2] == 0;
-
-            if (isMagenta)
-            {
-                var brightness = abgr[3] / 128.0;
-                pixel = (materialColor * brightness).value;
-            }
-
-            var targetY = y - source.getTop() + target.getTop();
-            var targetX = x - source.getLeft() + target.getLeft();
-            targetPixels[targetY * targetWidth + targetX] = pixel;
-        }
-    }
-}
-
-Vector2 Texture::getSize()
-{
-    return Vector2(surface->w, surface->h);
-}
-
-int Texture::getWidth()
-{
-    return surface->w;
-}
-
-int Texture::getHeight()
-{
-    return surface->h;
-}
-
-void Texture::setColor(Color32 color)
-{
-    SDL_SetSurfaceColorMod(surface.get(),
-                           byte(color.getRed()),
-                           byte(color.getGreen()),
-                           byte(color.getBlue()));
-    SDL_SetSurfaceAlphaMod(surface.get(),
-                           byte(color.getAlpha()));
 }
