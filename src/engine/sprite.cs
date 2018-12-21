175529e838eda42b61a260344b223fea0df47928
emlai <emil@cs.helsinki.fi>
2018-12-22 00:00:32 +0200

    Update sprite.cs

--- src/engine/sprite.cs
+++ src/engine/sprite.cs
@@ -1,21 +1,5 @@
 class Sprite
 {
-    Sprite(Texture texture, Rect textureRegion, char asciiGlyph, Color32 asciiColor,
-           Color32 materialColor = Color32::none, int animationFrames = 1);
-    Vector2 getSize();
-    int getWidth() { return getSize().x; }
-    int getHeight() { return getSize().y; }
-    void render(Window window, Vector2 position, Vector2 sourceOffset = Vector2(0, 0));
-    Color32 getMaterialColor() { return materialColor; }
-    void setMaterialColor(Color32 color) { materialColor = color; }
-    void setFrame(int newFrame) { frame = newFrame; }
-    void setAsciiGlyph(char glyph) { asciiGlyph = glyph; }
-    static bool useAsciiGraphics() { return asciiGraphics; }
-    static void useAsciiGraphics(bool asciiGraphics) { Sprite::asciiGraphics = asciiGraphics; }
-    static BitmapFont getAsciiGraphicsFont() { return asciiGraphicsFont; }
-    static void setAsciiGraphicsFont(BitmapFont font) { asciiGraphicsFont = font; }
-
-private:
     Texture texture;
     Rect textureRegion;
     Color32 materialColor;
@@ -25,45 +9,54 @@ private:
     Color32 asciiColor;
     static bool asciiGraphics;
     static BitmapFont asciiGraphicsFont;
-}
-bool Sprite::asciiGraphics;
-BitmapFont Sprite::asciiGraphicsFont;
 
-Sprite::Sprite(Texture texture, Rect textureRegion, char asciiGlyph, Color32 asciiColor,
-               Color32 materialColor, int animationFrames)
-:   texture(texture), textureRegion(textureRegion), materialColor(materialColor),
+    int getWidth() { return getSize().x; }
+    int getHeight() { return getSize().y; }
+    Color32 getMaterialColor() { return materialColor; }
+    void setMaterialColor(Color32 color) { materialColor = color; }
+    void setFrame(int newFrame) { frame = newFrame; }
+    void setAsciiGlyph(char glyph) { asciiGlyph = glyph; }
+    static bool useAsciiGraphics() { return asciiGraphics; }
+    static void useAsciiGraphics(bool asciiGraphics) { asciiGraphics = asciiGraphics; }
+    static BitmapFont getAsciiGraphicsFont() { return asciiGraphicsFont; }
+    static void setAsciiGraphicsFont(BitmapFont font) { asciiGraphicsFont = font; }
+
+    Sprite(Texture texture, Rect textureRegion, char asciiGlyph, Color32 asciiColor,
+        Color32 materialColor, int animationFrames)
+    :   texture(texture), textureRegion(textureRegion), materialColor(materialColor),
     animationFrames(animationFrames), frame(0), asciiGlyph(asciiGlyph), asciiColor(asciiColor)
-{
-}
-
-Vector2 Sprite::getSize()
-{
-    if (useAsciiGraphics())
-        return getAsciiGraphicsFont()->getCharSize();
-    else
-        return textureRegion.size;
-}
-
-void Sprite::render(Window window, Vector2 position, Vector2 sourceOffset)
-{
-    if (useAsciiGraphics())
     {
-        bool blend = false;
-        var font = getAsciiGraphicsFont();
-        font->setArea(Rect(position, getSize()));
-        font->print(window, string(asciiGlyph, 1), asciiColor, Color32::none, blend);
     }
-    else
-    {
-        int msPerAnimationFrame = window.getGraphicsContext().getAnimationFrameTime();
-        int animationFrame = SDL_GetTicks() / msPerAnimationFrame % animationFrames;
-        sourceOffset.x += (animationFrame + frame) * textureRegion.getWidth();
-        Rect source = textureRegion.offset(sourceOffset);
-        Rect target(position, textureRegion.size);
 
-        if (materialColor)
-            texture->render(window, source, target, materialColor);
+    Vector2 getSize()
+    {
+        if (useAsciiGraphics())
+            return getAsciiGraphicsFont()->getCharSize();
         else
-            texture->render(window, source, target);
+            return textureRegion.size;
+    }
+
+    void render(Window window, Vector2 position, Vector2 sourceOffset)
+    {
+        if (useAsciiGraphics())
+        {
+            bool blend = false;
+            var font = getAsciiGraphicsFont();
+            font->setArea(Rect(position, getSize()));
+            font->print(window, string(asciiGlyph, 1), asciiColor, Color32::none, blend);
+        }
+        else
+        {
+            int msPerAnimationFrame = window.getGraphicsContext().getAnimationFrameTime();
+            int animationFrame = SDL_GetTicks() / msPerAnimationFrame % animationFrames;
+            sourceOffset.x += (animationFrame + frame) * textureRegion.getWidth();
+            Rect source = textureRegion.offset(sourceOffset);
+            Rect target(position, textureRegion.size);
+
+            if (materialColor)
+                texture->render(window, source, target, materialColor);
+            else
+                texture->render(window, source, target);
+        }
     }
 }
