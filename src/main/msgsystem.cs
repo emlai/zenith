38781e5a51cf8e9ebc4f7db6cb156d68a0793207
emlai <emil@cs.helsinki.fi>
2018-12-22 01:18:41 +0200

    Update msgsystem.cs

--- src/main/msgsystem.cs
+++ src/main/msgsystem.cs
@@ -2,48 +2,35 @@ enum MessageType { Normal, Warning }
 
 class Message
 {
+    string text;
+    int turn;
+    int count;
+
     Message(string text, int turn) : text(text), turn(turn), count(1) {}
     string getText() { return text; }
     int getTurn() { return turn; }
     int getCount() { return count; }
     void increaseCount(int currentTurn) { ++count; turn = currentTurn; }
-    void save(SaveFile file);
-    static Message load(SaveFile file);
 
-private:
-    string text;
-    int turn;
-    int count;
+    void save(SaveFile file)
+    {
+        file.write(text);
+        file.writeInt32(turn);
+    }
+
+    Message load(SaveFile file)
+    {
+        var text = file.readString();
+        var turn = file.readInt32();
+        return Message(text, turn);
+    }
 }
 
-namespace MessageSystem
+static class MessageSystem
 {
     void drawMessages(Window window, BitmapFont, List<Message> messages,
                       int currentTurn);
 
-#ifdef DEBUG
-    void addDebugMessage(string message, MessageType = Normal);
-    void addToCommandHistory(string command);
-    string getPreviousCommand();
-    string getNextCommand();
-    void clearDebugMessageHistory();
-#endif
-}
-void Message::save(SaveFile file)
-{
-    file.write(text);
-    file.writeInt32(turn);
-}
-
-Message Message::load(SaveFile file)
-{
-    var text = file.readString();
-    var turn = file.readInt32();
-    return Message(text, turn);
-}
-
-namespace MessageSystem
-{
     const int maxMessagesToPrint = 6;
 
 #ifdef DEBUG
@@ -63,70 +50,70 @@ namespace MessageSystem
     static List<string> commandHistory;
     static List<string>::iterator commandIterator = commandHistory.end();
 #endif
-}
 
-void MessageSystem::drawMessages(Window window, BitmapFont font,
-                                 List<Message> messages, int currentTurn)
-{
-    font.setArea(GUI::getMessageArea(window));
-    for (int end = int(messages.size()), i = std::max(0, end - maxMessagesToPrint); i < end; ++i)
+    void MessageSystem::drawMessages(Window window, BitmapFont font,
+                                     List<Message> messages, int currentTurn)
     {
-        bool isNewMessage = messages[i].getTurn() >= currentTurn - 1;
-        var color = isNewMessage ? TextColor::White : TextColor::Gray;
-        font.print(window, "- ", color);
-        string text(messages[i].getText());
-        if (messages[i].getCount() > 1)
-            text += " (x" + std::to_string(messages[i].getCount()) + ")";
-        font.printLine(window, text, color);
+        font.setArea(GUI::getMessageArea(window));
+        for (int end = int(messages.size()), i = std::max(0, end - maxMessagesToPrint); i < end; ++i)
+        {
+            bool isNewMessage = messages[i].getTurn() >= currentTurn - 1;
+            var color = isNewMessage ? TextColor::White : TextColor::Gray;
+            font.print(window, "- ", color);
+            string text(messages[i].getText());
+            if (messages[i].getCount() > 1)
+                text += " (x" + std::to_string(messages[i].getCount()) + ")";
+            font.printLine(window, text, color);
+        }
+
+    #ifdef DEBUG
+        font.setArea(GUI::getDebugMessageArea(window));
+        foreach (DebugMessage message in debugMessages)
+            font.printLine(window, message.content, messageColors[message.type]);
+    #endif
     }
 
-#ifdef DEBUG
-    font.setArea(GUI::getDebugMessageArea(window));
-    foreach (DebugMessage message in debugMessages)
-        font.printLine(window, message.content, messageColors[message.type]);
-#endif
+    #ifdef DEBUG
+
+    void MessageSystem::addDebugMessage(string message, MessageType type)
+    {
+        debugMessages.push_front(DebugMessage(message, type));
+
+        if (debugMessages.size() > debugMessageLimit)
+            debugMessages.pop_back();
+    }
+
+    void MessageSystem::addToCommandHistory(string command)
+    {
+        if (commandHistory.empty() || commandHistory.back() != command)
+            commandHistory.push_back(command);
+
+        commandIterator = commandHistory.end();
+    }
+
+    string MessageSystem::getPreviousCommand()
+    {
+        if (commandHistory.empty())
+            return "";
+
+        if (commandIterator != commandHistory.begin())
+            --commandIterator;
+
+        return commandIterator;
+    }
+
+    string MessageSystem::getNextCommand()
+    {
+        if (commandIterator == commandHistory.end() || ++commandIterator == commandHistory.end())
+            return "";
+
+        return commandIterator;
+    }
+
+    void MessageSystem::clearDebugMessageHistory()
+    {
+        debugMessages.clear();
+    }
+
+    #endif
 }
-
-#ifdef DEBUG
-
-void MessageSystem::addDebugMessage(string message, MessageType type)
-{
-    debugMessages.push_front(DebugMessage(message, type));
-
-    if (debugMessages.size() > debugMessageLimit)
-        debugMessages.pop_back();
-}
-
-void MessageSystem::addToCommandHistory(string command)
-{
-    if (commandHistory.empty() || commandHistory.back() != command)
-        commandHistory.push_back(command);
-
-    commandIterator = commandHistory.end();
-}
-
-string MessageSystem::getPreviousCommand()
-{
-    if (commandHistory.empty())
-        return "";
-
-    if (commandIterator != commandHistory.begin())
-        --commandIterator;
-
-    return commandIterator;
-}
-
-string MessageSystem::getNextCommand()
-{
-    if (commandIterator == commandHistory.end() || ++commandIterator == commandHistory.end())
-        return "";
-
-    return commandIterator;
-}
-
-void MessageSystem::clearDebugMessageHistory()
-{
-    debugMessages.clear();
-}
-
-#endif
