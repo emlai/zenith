d9227305a311be7dd0636b0016826028705abff2
emlai <emil@cs.helsinki.fi>
2019-01-04 03:08:46 +0200

    Allow customizing movement keys, default to WASD

--- src/main/action.cpp
+++ src/main/action.cpp
@@ -2,9 +2,58 @@
 #include "creature.h"
 #include "engine/assert.h"
 
+const Action movementActions[8] = { MoveUp, MoveUpLeft, MoveLeft, MoveDownLeft, MoveDown, MoveDownRight, MoveRight, MoveUpRight };
+
 static bool isMovementAction(Action action)
 {
-    return action == Move || action == GoUpOrDown;
+    switch (action)
+    {
+        case MoveUp:
+        case MoveUpLeft:
+        case MoveLeft:
+        case MoveDownLeft:
+        case MoveDown:
+        case MoveDownRight:
+        case MoveRight:
+        case MoveUpRight:
+        case GoUpOrDown:
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+Action getMovementAction(Dir8 direction)
+{
+    switch (direction)
+    {
+        case North: return MoveUp;
+        case NorthWest: return MoveUpLeft;
+        case West: return MoveLeft;
+        case SouthWest: return MoveDownLeft;
+        case South: return MoveDown;
+        case SouthEast: return MoveDownRight;
+        case East: return MoveRight;
+        case NorthEast: return MoveUpRight;
+        default: ASSERT(false);
+    }
+}
+
+Dir8 getMovementDirection(Action action)
+{
+    switch (action)
+    {
+        case MoveUp: return North;
+        case MoveUpLeft: return NorthWest;
+        case MoveLeft: return West;
+        case MoveDownLeft: return SouthWest;
+        case MoveDown: return South;
+        case MoveDownRight: return SouthEast;
+        case MoveRight: return East;
+        case MoveUpRight: return NorthEast;
+        default: ASSERT(false);
+    }
 }
 
 const char* toString(Action action)
@@ -12,7 +61,14 @@ const char* toString(Action action)
     switch (action)
     {
         case Wait: return "Wait";
-        case Move: return "Move";
+        case MoveUp: return "Up";
+        case MoveUpLeft: return "UpLeft";
+        case MoveLeft: return "Left";
+        case MoveDownLeft: return "DownLeft";
+        case MoveDown: return "Down";
+        case MoveDownRight: return "DownRight";
+        case MoveRight: return "Right";
+        case MoveUpRight: return "UpRight";
         case GoUpOrDown: return "GoUpOrDown";
         case Attack: return "Attack";
         case PickUpItems: return "PickUp";
